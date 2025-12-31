442717f48670e0f4760659eb0f5739905bb8f85b
ahshobuj1 <ahshobuj09@gmail.com>
2026-01-01 05:47:03 +0600

    latest update

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,10 +2,7 @@ import express from 'express';
 import {userService} from './user.service';
 
 const createAdmin = async (req: express.Request, res: express.Response) => {
-  // console.log(req.body);
   const result = await userService.createAdmin(req.body);
-  console.log(result);
-
   return res.json(result);
 };
 
